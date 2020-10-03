daf5ae3d0f4db05ad7b3f194ef2353d28dccdfc7
JulienDaCosta-GitHub <55091161+JulienDaCosta-GitHub@users.noreply.github.com>
2020-10-03 16:26:42 +0200

    Checklist

--- plateforme_elearning/src/index.js
+++ plateforme_elearning/src/index.js
@@ -10,3 +10,105 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+class AppContainer extends React.Component {
+  constructor (props){
+    super ();
+  }
+  render (){
+    return (
+      <div>
+        <AppJumbotron title="Objectifs" />
+        <ItemList />
+        <br />
+        <br />
+        <br />
+        <ItemCount count={allTheThings.length} />
+        <hr />
+        <AppFooter />
+      </div>
+    );
+  }
+}
+
+class Item extends React.Component {
+  constructor (props){
+    super ();
+
+    this.state = {
+      checked: false
+    };
+
+    this.handleClick = this.handleClick.bind(this);    
+  }
+  handleClick (e){
+    this.setState({
+      checked: !this.state.checked
+    });
+
+  }
+  render (){
+    let text = this.state.checked ? <strike>{this.props.message}</strike> : this.props.message;
+    return (
+        <div className="row">
+          <div className="col-md-12">
+            <input type="checkbox" onClick={this.handleClick} />&nbsp;{text}
+            <hr />
+          </div>
+        </div>
+    );
+  }
+}
+
+let item2 = <Item message="Changer la couleur de l'objet" />;
+let item3 = <Item message="Déplacer l'objet à droite" />;
+let item4 = <Item message="Faire une rotation à 90° de l'objet" />;
+
+let allTheThings = [item2, item3, item4];
+
+class ItemList extends React.Component {
+  constructor (props){
+    super ();
+  }
+  render (){
+    let items = allTheThings.map(thing => thing);
+    return (
+        <h4>{items}</h4>
+    );
+  }
+}
+
+class ItemCount extends React.Component {
+  constructor (props){
+    super ();
+  }
+  render (){
+    return (
+      <h4>Il y a {this.props.count} objectifs sur cette liste.</h4>
+    );
+  }
+}
+
+class AppJumbotron extends React.Component {
+  render (){
+    return (
+      <div className="jumbotron">
+        <h2>{this.props.title}</h2>
+      </div>
+    );
+  }
+}
+
+class AppFooter extends React.Component {
+  render (){
+    return (
+      <div className="text-muted">
+        <small>&copy; {new Date().getFullYear()}</small>
+      </div>
+    );
+  }
+}
+
+let target = document.getElementById('app');
+ReactDOM.render(<AppContainer />, target);
+
+
